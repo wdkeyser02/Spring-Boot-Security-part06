397692d5354ad2de9a99fde1e543dfa1cdcc38c8
willydekeyser <ma074172@skynet.be>
2025-12-05 11:42:19 +0100

    Initial commit

---
+++ src/main/java/willydekeyser/springbootsecuritypart06/SpringBootSecurityPart06Application.java
@@ -0,0 +1,13 @@
+package willydekeyser.springbootsecuritypart05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityPart05Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityPart05Application.class, args);
+    }
+
+}
---
+++ src/test/java/willydekeyser/springbootsecuritypart02/SpringBootSecurityPart01ApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser.springbootsecuritypart02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityPart01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
